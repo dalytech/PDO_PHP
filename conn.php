3f365dccd89bed2f904798e23c11eb80cc8230f8
dalytech <49926562+dalytech@users.noreply.github.com>
2021-08-27 14:45:32 +0100

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,7 +1,7 @@
 <?php
-    $db_username = 'root';
-    $db_password = '';
-    $conn = new PDO( 'mysql:host=localhost;dbname=db_pdo_crud', $db_username, $db_password );
+    $db_username = 'MKqcFG1GSF';
+    $db_password = 'lzWlhmmNhl';
+    $conn = new PDO( 'mysql:host=remotemysql.com;dbname=MKqcFG1GSF', $db_username, $db_password );
     if(!$conn){
         die("Fatal Error: Connection Failed!");
     }
